904b1df143c2be27788470e99d029bb6e6eb4058
Altison <71751499+Altison@users.noreply.github.com>
2020-10-14 16:04:02 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-def on_forever():
+def on_button_pressed_a():
     for y in range(6):
         for x in range(6):
-            led.plot(0, y)
+            if y == 1:
+                continue
+            led.plot(x, y)
             basic.pause(100)
-    basic.pause(100)
+    basic.pause(2000)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
     for x2 in range(6):
         for y2 in range(6):
-            led.unplot(5 - x2, 5 - y2)
+            if x2 % 2 == 1:
+                led.plot(5 - x2, 5 - y2)
+            else:
+                led.plot(5 - x2, 0)
+                led.unplot(0, y2)
+            basic.pause(100)
     basic.pause(100)
-basic.forever(on_forever)
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     for (let y = 0; y <= 5; y++) {
         for (let x = 0; x <= 5; x++) {
             if (y == 1) {
-                break;
+                continue;
             }
             led.plot(x, y)
             basic.pause(100)
@@ -12,22 +12,14 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
     for (let x = 0; x <= 5; x++) {
         for (let y = 0; y <= 5; y++) {
             if (x % 2 == 1) {
-                led.unplot(5 - x, 5 - y)
-                basic.pause(100)
+                led.plot(5 - x, 5 - y)
             } else {
-                led.unplot(5 - x, y)
-                basic.pause(100)
+                led.plot(5 - x, y)
             }
+            basic.pause(100)
         }
     }
     basic.pause(100)
