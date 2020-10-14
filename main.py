30400563d4a8981ad8eda799dec45fc4a95b62e0
Altison <71751499+Altison@users.noreply.github.com>
2020-10-14 15:29:39 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ degree = 0
 def on_forever():
     global degree
     degree = input.compass_heading()
+    if degree < 45 or degree >= 315:
+        basic.show_string("N")
+    elif degree < 135:
+        basic.show_string("E")
+    elif degree < 225:
+        basic.show_string("S")
+    else:
+        basic.show_string("W")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
-let degree = 0
 basic.forever(function () {
-    degree = input.compassHeading()
-    if (degree < 45 || degree >= 315) {
-        basic.showString("N")
-    } else if (degree < 135) {
-        basic.showString("E")
-    } else if (degree < 225) {
-        basic.showString("S")
-    } else {
-        basic.showString("W")
+    for (let index = 0; index <= 4; index++) {
+        if (index == 4) {
+            continue;
+        }
+        basic.showNumber(index)
+        basic.pause(100)
     }
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
 })
