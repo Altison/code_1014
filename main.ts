64b28a582d8425d668613427251f9334cf47e42d
Altison <71751499+Altison@users.noreply.github.com>
2020-10-14 15:44:55 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,17 @@ input.onButtonPressed(Button.A, function () {
             basic.pause(100)
         }
     }
-    basic.pause(100)
+    basic.pause(2000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
     for (let x = 0; x <= 5; x++) {
         for (let y = 0; y <= 5; y++) {
             if (x % 2 == 1) {
